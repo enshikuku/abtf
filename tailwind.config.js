e8bb5691104b037c8f7c55deb75e65fae5a86da0
enshikuku <enshikuku@gmail.com>
2026-03-10 14:24:44 -0400

    update: tailwind.config.js changes

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+  './index.html',
+  './src/**/*.{js,ts,jsx,tsx}'
+],
+  theme: {
+    extend: {
+      colors: {
+        deepBlue: '#000033',
+        maroon: '#660000',
+        gold: '#996633',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
